ff7b66e7a669df73bd4018c241efcb3fb38b500c
ryley <rreid73>
2018-11-06 03:01:34 +0000

    Commit

---
+++ assign2.sh
@@ -0,0 +1,5 @@
+sudo mysql --verbose -psherman < rreid73part1.txt  > rreid73outputscript1.txt 2>&1
+sudo mysql --verbose -psherman < rreid73part2.txt  > rreid73outputscript2.txt 2>&1
+sudo mysql --verbose -psherman < rreid73part3.txt  > rreid73outputscript3.txt 2>&1
+sudo mysql --verbose -psherman < rreid73part4.txt  > rreid73outputscript4.txt 2>&1
+
